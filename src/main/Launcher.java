3b01d68abf1baad45044aa00a1cf2ee381098640
Job <job.van.der.meiden@hva.nl>
2019-06-24 09:36:00 +0200

    help command gemaakt

--- src/main/Launcher.java
+++ src/main/Launcher.java
@@ -8,13 +8,17 @@ public class Launcher {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        Board board = new Board(0,0);
+        Board board = new Board(0, 0);
         board.printBoard();
         boolean keepOnGoing = true;
-        while (keepOnGoing){
+        while (keepOnGoing) {
+            System.out.print("Input (h for help): ");
             String in = input.next();
-            if (in.equals("e")){
+            if (in.equals("e")) {
                 break;
+            } else if (in.equals("h")) {
+                System.out.println("lopen: u / d / l / r, exit: e");
+                continue;
             }
             board.walk(in);
         }
